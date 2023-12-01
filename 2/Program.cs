6bbfa6d7ba311507678d27dd7059b908eeae1fe8
sysoeva svetlana <s9192489459@gmail.com>
2023-12-01 20:26:43 +0300

    first

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт
+// сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int S = 0;
+Sum(M,N,S);
+void Sum(int M, int N, int S){
+    if (M>N){
+        Console.Write(S);
+        return;
+    }
+    else{
+        S=S+M;
+        M=M+1;
+        Sum(M,N,S);
+
+    }
+}
---
+++ 3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Arc(N, M));
+int Arc(int N, int M)
+{
+  if (N == 0)
+  {
+    return M + 1;}
+  else{
+    if ((N != 0) && (M == 0)){
+      return Arc(N - 1, 1);}
+    else{
+      return Arc(N - 1, Arc(N, M - 1));}}
+}
